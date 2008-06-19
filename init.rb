c0b4f52762e39dd1bb5153c0b0e099f9ab2b735e
David Rice <dave@starscream.local>
2008-06-19 18:25:15 +0100

    Pushing to GitHub

---
+++ init.rb
@@ -0,0 +1,34 @@
+require 'exceptional/agent'
+
+logger = RAILS_DEFAULT_LOGGER
+Exceptional::Agent.api_key = nil
+config_file = "#{RAILS_ROOT}/config/exceptional.yml"
+load_error = nil
+
+mongrel = nil;
+
+if defined?(Mongrel)
+  ObjectSpace.each_object(Mongrel::HttpServer) do |mongrel_instance|
+    agent.log.info("Multiple Mongrels in the Ruby vm? this might not work!") if mongrel  
+    mongrel = mongrel_instance
+  end
+end
+
+if FileTest.exists?(config_file)
+  # Load exceptional config file
+  config = YAML::load(File.open(config_file))
+  Exceptional::Agent.api_key = config['api-key']
+else
+  load_error = "WARNING: Exceptional config file not found (RAILS_ROOT/config/exceptional.yml)"
+end
+
+logger.fatal "*"*78
+unless load_error 
+  logger.fatal "** Exceptional plugin loaded"
+  logger.fatal "** API key: " + Exceptional::Agent.api_key.inspect
+  logger.fatal "** Mongrel (port): " + mongrel.port.to_s if mongrel
+else
+  logger.fatal "** Exceptional plugin not loaded"
+  logger.fatal "** " + load_error 
+end
+logger.fatal "*"*78
---
+++ lib/exceptional/agent.rb
@@ -0,0 +1,73 @@
+require 'net/https' 
+require 'net/http'
+require 'builder'
+
+module Exceptional
+  
+  PROTOCOL_VERSION = 1
+  REMOTE_HOST = "getexceptional.com"
+  REMOTE_PORT = 80
+  
+  class Agent  
+    
+    cattr_accessor :api_key
+    
+    
+    # TODO make controller and request optional to handle errors in daemons
+    def self.send_data(exception, controller, request)
+      xml = prepare_xml(exception,controller,request)
+      http = Net::HTTP.new(REMOTE_HOST, REMOTE_PORT.to_i) 
+      uri = "/errors?&api_key=#{Exceptional::Agent.api_key}&protocol_version=#{PROTOCOL_VERSION}"
+      headers = { 'Content-Type' => 'application/xml', 'Accept' => 'application/xml' }
+      response = http.start do |http|
+        http.post(uri, xml, headers) 
+      end
+    end
+    
+    protected
+    
+    def self.prepare_xml(exception,controller,request)
+      xml = Builder::XmlMarkup.new
+      xml.instruct! 
+      xml.error do |e|
+        e.api_key Exceptional::Agent.api_key
+        e.controller_name controller.controller_name
+        e.action_name controller.action_name
+        e.backtrace exception.backtrace.join("\n")
+        e.user_ip request.env["HTTP_X_FORWARDED_FOR"] || request.env["REMOTE_ADDR"]
+        e.host_ip request.env["HTTP_HOST"]
+        e.environment prepare_environment_to_send(request.env)
+        e.session prepare_session_to_send(request.session)
+        e.request prepare_request_to_send(request)
+        e.occurred_at Time.now.to_s
+        e.summary "#{controller.controller_name}##{controller.action_name} (#{exception.class}) #{exception.message.inspect}"
+      end
+      xml.target!
+    end
+    
+    def self.prepare_environment_to_send(environment)
+      env = ""
+      environment.keys.sort.each do |key|
+        env << "#{key}:, #{environment[key].to_s.strip}\n"
+      end
+      env
+    end
+
+    def self.prepare_request_to_send(request)
+      req = ""
+      req << "URL: #{request.protocol}#{request.env["HTTP_HOST"]}#{request.request_uri}\n"
+      req << "Parameters: #{request.parameters.inspect}\n"
+      req << "Rails root: #{RAILS_ROOT}"
+      req
+    end
+
+    def self.prepare_session_to_send(session)
+      ses = ""
+      for variable in session.instance_variables
+        next if variable =~ /^@db/
+        ses << "#{variable}: #{session.instance_variable_get(variable)}\n"
+      end
+      ses
+    end
+  end
+end
