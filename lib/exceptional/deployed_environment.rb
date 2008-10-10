d3952eb01ca7437ee05dc1ba8b462b290e6bccc8
David Rice <dave@starscream-2.local>
2008-10-10 14:37:04 +0100

    Fix scoping for DeployedEnvironment

--- lib/exceptional/deployed_environment.rb
+++ lib/exceptional/deployed_environment.rb
@@ -1,39 +1,40 @@
 # This class is used to determine the server environment
-class DeployedEnvironment
-    
+module Exceptional
+  class DeployedEnvironment
+  
     attr_reader :server, :identifier, :hostname#, :environment
-    
+  
     def initialize
       @server = :unknown
       @identifier = nil
-      
+    
       @hostname = determine_host
       # @environment = Exceptional.environment
       # @application_root = Exceptional.application_root
-      
+    
       servers = %w[webrick mongrel thin litespeed passenger]
       while servers.any? && @identifier.nil?
         send 'is_'+(servers.shift)+'?'
       end
-      
-    end
     
+    end
+  
     def should_start_worker?
       !identifier.nil?
     end
-    
+  
     def determine_mode
       @server == :passenger ? :direct : :queue      
     end
-    
+  
     def to_s
       "#{@hostname}:#{@identifier} [#{@server}]"
     end
-    
+  
     def determine_host
       Socket.gethostname
     end
-    
+  
     def is_webrick?
       if defined?(OPTIONS) && defined?(DEFAULT_PORT) && OPTIONS.respond_to?(:fetch) 
         # OPTIONS is set by script/server if launching webrick
@@ -41,7 +42,7 @@ class DeployedEnvironment
         @server = :webrick
       end
     end
-    
+  
     def is_mongrel?
       if defined? Mongrel::HttpServer
         ObjectSpace.each_object(Mongrel::HttpServer) do |mongrel|
@@ -51,7 +52,7 @@ class DeployedEnvironment
         end
       end
     end
-    
+  
     def is_thin?
       if defined? Thin::Server
         ObjectSpace.each_object(Thin::Server) do |thin_server|
@@ -65,19 +66,19 @@ class DeployedEnvironment
         end
       end
     end
-        
+      
     def is_litespeed?
       if caller.pop =~ /fcgi-bin\/RailsRunner\.rb/
         @server = :litespeed
         @identifier = 'litespeed'
       end
     end
-    
+  
     def is_passenger?
       if defined? Passenger::AbstractServer
         @server = :passenger
         @identifier = 'passenger'
       end
     end
-    
+  end
 end
