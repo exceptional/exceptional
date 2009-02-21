bb1e030ec81db4965b3c2e33351b6946db845cfa
Ladislav Martincik <ladislav.martincik@gmail.com>
2009-02-21 17:56:45 +0100

    - Fixed broken test build

--- spec/exceptional_spec.rb
+++ spec/exceptional_spec.rb
@@ -12,7 +12,8 @@ describe Exceptional do
     end
     
     it "should parse exception into exception data object" do
-     exception = mock(Exception, :message => "Something bad has happened", :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"])
+     exception = mock(Exception, :message => "Something bad has happened", 
+      :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"])
      exception_data = Exceptional.parse(exception)
      exception_data.kind_of?(Exceptional::ExceptionData).should be_true
      exception_data.exception_message.should == exception.message
@@ -21,21 +22,33 @@ describe Exceptional do
     end
     
     it "should post exception" do
-      exception_data = mock(Exceptional::ExceptionData, :message => "Something bad has happened", :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], :class => Exception)
+      exception_data = mock(Exceptional::ExceptionData, 
+        :message => "Something bad has happened", 
+        :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], 
+        :class => Exception, :to_hash => { :message => "Something bad has happened" })
       Exceptional.should_receive(:call_remote, :with => [:errors, exception_data])
       Exceptional.post(exception_data)
     end
     
     it "should catch exception" do
-      exception = mock(Exception, :message => "Something bad has happened", :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"])
-      exception_data = mock(Exceptional::ExceptionData, :message => "Something bad has happened", :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], :class => Exception)
+      exception = mock(Exception, :message => "Something bad has happened", 
+        :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"])
+      exception_data = mock(Exceptional::ExceptionData, 
+        :message => "Something bad has happened", 
+        :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], 
+        :class => Exception, :to_hash => { :message => "Something bad has happened" })
+      exception_data.should_receive(:controller_name=).with("spec")
       Exceptional.should_receive(:parse, :with => [exception]).and_return(exception_data)
       Exceptional.should_receive(:post, :with => [exception_data])
       Exceptional.catch(exception)
     end
     
     it "should raise a license exception if api key is not set" do
-      exception_data = mock(Exceptional::ExceptionData, :message => "Something bad has happened", :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], :class => Exception)
+      exception_data = mock(Exceptional::ExceptionData, 
+        :message => "Something bad has happened", 
+        :backtrace => ["/app/controllers/buggy_controller.rb:29:in `index'"], 
+        :class => Exception,
+        :to_hash => { :message => "Something bad has happened" })
       Exceptional.api_key.should == nil
       lambda { Exceptional.post(exception_data) }.should raise_error(Exceptional::LicenseException)    
     end
