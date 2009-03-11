aef9158409b1b7ab6a5594adeeba8679afb0bb56
David Rice <me@davidjrice.co.uk>
2009-03-11 22:41:46 +0000

    Add TODO about change where exceptional hooks into rails

--- lib/exceptional/integration/rails.rb
+++ lib/exceptional/integration/rails.rb
@@ -4,6 +4,7 @@ module ActionController
   class Base
     
     def rescue_action_with_exceptional(exception)
+      # TODO potentially hook onto rescue_without_handler if it exists? would negate need to check handler_for_rescue every time.
       unless respond_to?(:handler_for_rescue) && handler_for_rescue(exception) # if there's handler defined with rescue_from() do not call Exceptional
         params_to_send = (respond_to? :filter_parameters) ? filter_parameters(params) : params
         Exceptional.handle(exception, self, request, params_to_send)
