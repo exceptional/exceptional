51c601e4fc5ba36d7178b94cdcb97a0fc3c31dc2
wal <walter.mcconnell@gmail.com>
2009-03-16 16:41:10 +0000

    fix up init.rb

--- init.rb
+++ init.rb
@@ -1,19 +1,20 @@
 require 'exceptional'
 
+
 def to_stderr(s)
   STDERR.puts "** [Exceptional] " + s
 end
 
 config_file = File.join(RAILS_ROOT,"/config/exceptional.yml")
 
-begin 
-  Config.load_config(config_file, RAILS_ENV, RAILS_ROOT)
-  
-  if Config.enabled?
+begin
+  Exceptional::Config.load_config(config_file, RAILS_ENV, RAILS_ROOT)
+
+  if Exceptional::Config.enabled?
     Exceptional.startup
   end
-  
+
 rescue Exception => e
   to_stderr e
-  to_stderr "Exceptional Plugin disabled."
+  to_stderr "Plugin disabled."
 end
