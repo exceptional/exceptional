c6684f17d5515f1fa56dc13eb5d216bac873db27
wal <walter.mcconnell@gmail.com>
2009-04-05 14:03:38 +0100

    logging tidyup

--- lib/exceptional/adapters/http_async_adapter.rb
+++ lib/exceptional/adapters/http_async_adapter.rb
@@ -10,12 +10,17 @@ module Exceptional
     class HttpAsyncAdapter < BaseAdapter
 
       include Exceptional::Utils::HttpUtils
-      
+
       def publish_exception(json_data)
         begin
+          
           # Temporarily create a Thread just for this send
           Thread.new {
-            http_call_remote(:errors, json_data)
+            begin
+              http_call_remote(:errors, json_data)
+            rescue Exception => e
+              Exceptional.log! e.message
+            end
           }
         rescue Exception => e
           raise HttpAsyncAdapterException.new e.message
