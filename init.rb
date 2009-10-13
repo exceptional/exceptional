c246dcf7998348123860fe7a5b559c0353bfa62f
Darragh Curran <darragh@peelmeagrape.net>
2009-10-13 17:40:21 +0100

    rubygems config tidyup

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
-require 'exceptional'
-
-begin
-  config_file = "#{RAILS_ROOT}/config/exceptional.yml"
-  Exceptional::Config.load(RAILS_ROOT, RAILS_ENV, config_file)
-  Exceptional::Startup.announce
-  require File.join('exceptional', 'integration', 'rails')
-rescue => e
-  STDERR.puts "Problem starting Exceptional Plugin. Your app will run as normal."
-  STDERR.puts e
-end
+#require 'exceptional'
+#
+#begin
+#  config_file = "#{RAILS_ROOT}/config/exceptional.yml"
+#  Exceptional::Config.load(RAILS_ROOT, RAILS_ENV, config_file)
+#  Exceptional::Startup.announce
+#  require File.join('exceptional', 'integration', 'rails')
+#rescue => e
+#  STDERR.puts "Problem starting Exceptional Plugin. Your app will run as normal."
+#  STDERR.puts e
+#end
