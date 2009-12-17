1ffabb3d56f8bc31d95ebc430f622b25becb48dc
Andrew Grim <andrew@kongregate.com>
2009-12-17 12:40:16 -0800

    ruby 1.9 compat

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'exceptional'
 # If old plugin still installed then we don't want to install this one.
 # In production environments we should continue to work as before, but in development/test we should
 # advise how to correct the problem and exit
-if defined?(Exceptional::VERSION::STRING) && %w(development test).include?(RAILS_ENV)
+if (defined?(Exceptional::VERSION::STRING) rescue nil) && %w(development test).include?(RAILS_ENV)
   message = %Q(
   ***********************************************************************
   You seem to still have an old version of the exceptional plugin installed.
