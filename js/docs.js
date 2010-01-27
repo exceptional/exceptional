e34822e5c3f3fc9804c9784dd42d4af27fd5b91d
David Barrett <dave@antidis.com>
2010-01-27 13:32:28 +0000

    Tightened rules for highlighting

--- js/docs.js
+++ js/docs.js
@@ -2,7 +2,6 @@ $(document).ready( function() {
   
   // Highlight correct TOC line
   var toc_url = location.href.replace(/^http\:\/\/[^\/]+\//, '');
-  
-  $("div#TableOfContents a[href*='" + toc_url + "']").addClass('selected');
+  $("div#TableOfContents a[href='/" + toc_url + "']").addClass('selected');
   
 });
