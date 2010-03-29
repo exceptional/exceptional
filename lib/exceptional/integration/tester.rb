2a45f6d00b0ae4d688a5a7bbc37c2394a43f53f9
wal <walter.mcconnell@gmail.com>
2010-03-29 23:15:40 +0100

    small bit of copy

--- lib/exceptional/integration/tester.rb
+++ lib/exceptional/integration/tester.rb
@@ -10,7 +10,7 @@ module Exceptional
         unless Exceptional::Remote.error(Exceptional::ExceptionData.new(e, "Test Exception"))
           puts "Problem sending exception to Exceptional. Check your API key."
         else
-          puts "Exception sent successfully."
+          puts "Test Exception sent. Please login to http://getexceptional.com to see it!"
         end
       end
     end
