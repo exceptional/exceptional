34d28adcaafe6e7eea1462b8916809de741df5ae
Ciaran Lee <ciaran.lee@gmail.com>
2010-12-01 15:26:02 +0000

    removing a debugging message from a spec

--- spec/exceptional/alert_exception_data_spec.rb
+++ spec/exceptional/alert_exception_data_spec.rb
@@ -4,7 +4,6 @@ describe Exceptional::AlertData do
   it "raises error" do
     data = Exceptional::AlertData.new(Exceptional::Alert.new("A string"), "Alert")
     result_json = JSON.parse(data.to_json)
-    puts result_json.inspect
     result_json['rescue_block']['name'].should == 'Alert'
     result_json['exception']['message'].should == "A string"
     result_json['exception']['exception_class'] == 'Exceptional::Alert'
