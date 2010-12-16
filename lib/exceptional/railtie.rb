a86ecf30d039f96177254b8ec11748c8dcb52116
Wal McConnell <walter.mcconnell@gmail.com>
2010-12-16 18:16:03 +0000

    tidy up

--- lib/exceptional/railtie.rb
+++ lib/exceptional/railtie.rb
@@ -6,11 +6,9 @@ module Exceptional
 
     initializer "exceptional.middleware" do |app|
 
-      if File.exist?(File.join(Rails.root, "/config/exceptional.yml"))
-        Exceptional::Config.load(File.join(Rails.root, "/config/exceptional.yml"))
-      else
-        # On Heroku config is loaded via the ENV
-      end
+      config_file = File.join(Rails.root, "/config/exceptional.yml")
+      Exceptional::Config.load config_file if File.exist?(config_file)
+      # On Heroku config is loaded via the ENV so no need to load it from the file
 
       if Exceptional::Config.should_send_to_api?
         Exceptional.logger.info("Loading Exceptional #{Exceptional::VERSION} for #{Rails::VERSION::STRING}")
