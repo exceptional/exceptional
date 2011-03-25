fdd4dacfc790b612b7489142023526861b6176d6
Arun Agrawal <arun@fromjaipur.com>
2011-03-25 13:26:24 +0530

    Missing Should added in alert data spec.

--- spec/exceptional/alert_exception_data_spec.rb
+++ spec/exceptional/alert_exception_data_spec.rb
@@ -6,6 +6,6 @@ describe Exceptional::AlertData do
     result_json = JSON.parse(data.to_json)
     result_json['rescue_block']['name'].should == 'Alert'
     result_json['exception']['message'].should == "A string"
-    result_json['exception']['exception_class'] == 'Exceptional::Alert'
+    result_json['exception']['exception_class'].should == 'Exceptional::Alert'
   end
 end
