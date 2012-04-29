603547febf5a1814248fa4e7bf015ceed4ed4abe
John Joseph Bachir <j@jjb.cc>
2012-04-29 15:09:51 -0400

    never swallow exceptions

--- lib/exceptional/integration/dj.rb
+++ lib/exceptional/integration/dj.rb
@@ -1,12 +1,9 @@
-begin
-  class Delayed::Job
-    def log_exception_with_exceptional(e)
-      Exceptional.handle(e, "Delayed::Job #{self.name}")
-      log_exception_without_exceptional(e)
-      Exceptional.context.clear!
-    end
-    alias_method_chain :log_exception, :exceptional
-    Exceptional.logger.info "DJ integration enabled"
+class Delayed::Job
+  def log_exception_with_exceptional(e)
+    Exceptional.handle(e, "Delayed::Job #{self.name}")
+    log_exception_without_exceptional(e)
+    Exceptional.context.clear!
   end
-rescue
+  alias_method_chain :log_exception, :exceptional
+  Exceptional.logger.info "DJ integration enabled"
 end
